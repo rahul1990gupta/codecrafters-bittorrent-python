9a2f2efa3a0519014bab1e0f863575d39a3a17f6
Rahul Gupta <rahul1990gupta@gmail.com>
2024-09-21 14:47:37 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import sys
 from app.decoder import BencodeDecoder as B
 from app.encode import encode as e
+import requests
 # import bencodepy - available if you need it!
 # import requests - available if you need it!
 
@@ -48,6 +49,9 @@ def main():
     command = sys.argv[1]
 
     # You can use print statements as follows for debugging, they'll be visible when running tests.
+    if command in ["info", "peers"]:
+        process_fc(command, sys.argv[2])
+        return
 
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
@@ -66,28 +70,37 @@ def main():
         # Uncomment this block to pass the first stage
         val = B(return_str=True).decode(bencoded_value)
         print(json.dumps(val, default=bytes_to_str))
-    elif command == "info":
-        metafile = sys.argv[2]
-        with open(metafile, 'rb') as f: 
-            file_dict = B().decode(f.read())
-            info_dict = file_dict[b"info"]
-            # import pdb;pdb.set_trace()
-            print("Tracker URL:", file_dict[b"announce"].decode().strip())
-            print("Length:", info_dict[b"length"])
-            
-            be_dict = e(info_dict)
-            import hashlib
-            print("Info Hash:", hashlib.sha1(be_dict).hexdigest())
-            print("Piece Length:", info_dict[b"piece length"])
-            print("Piece Hashes:")
-            i = 0 
-            cp = info_dict[b"pieces"]
-            
-            while i < len(cp):
-                ph = cp[i:i + 20]
-                print(ph.hex())
-                i+=20
+    else:
+        raise NotImplementedError(f"Unknown command {command}")
+
+
+def process_fc(command, metafile):
+    file_dict = {}
+    info_dict = {}
+    with open(metafile, 'rb') as f: 
+        file_dict = B().decode(f.read())
+        info_dict = file_dict[b"info"]
+    be_dict = e(info_dict)
+    import hashlib
+ 
+    info_hash = hashlib.sha1(be_dict)
+    tracker_url = file_dict[b"announce"].decode().strip()
     
+    if command == "info":
+        print("Tracker URL:", tracker_url)
+        print("Length:", info_dict[b"length"])
+        
+        print("Info Hash:", info_hash.hexdigest())
+        print("Piece Length:", info_dict[b"piece length"])
+        print("Piece Hashes:")
+        i = 0 
+        cp = info_dict[b"pieces"]
+        
+        while i < len(cp):
+            ph = cp[i:i + 20]
+            print(ph.hex())
+            i+=20
+
             """
             To calculate the info hash, you'll need to:
 
@@ -95,8 +108,33 @@ def main():
                 Bencode the contents of the info dictionary
                 Calculate the SHA-1 hash of this bencoded dictionary
             """
+    elif command == "peers":
+        payload = {
+                "info_hash": info_hash.digest(),
+                "peer_id": "00112233445566778899",
+                "port": 6881,
+                "uploaded": 0,
+                "downloaded": 0,
+                "left": info_dict[b"length"],
+                "compact": 1
+        }
+        response = requests.get(tracker_url, params=payload)
+        response_bd = B().decode(response.content)
+        peers = response_bd[b"peers"]
+        i = 0
+        while i < len(peers):
+            peer = peers[i:i+6]
+            p1 = peer[0]
+            p2 = peer[1]
+            p3 = peer[2]
+            p4 = peer[3]
+
+            port = int.from_bytes(peer[4:], 'big')
+            print(f"{p1}.{p2}.{p3}.{p4}:{port}")
+            i += 6
     else:
         raise NotImplementedError(f"Unknown command {command}")
+
 
 
 if __name__ == "__main__":
